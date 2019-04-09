dd49f12b8ab8dbe9fb43366e5e96ec12d52751b3
Mauricio Gomes <mauricio@edge14.com>
2019-04-09 18:02:45 -0400

    Add support for quick replies and buttons

--- lib/stealth/services/twilio/reply_handler.rb
+++ lib/stealth/services/twilio/reply_handler.rb
@@ -4,8 +4,9 @@
 module Stealth
   module Services
     module Twilio
-
       class ReplyHandler < Stealth::Services::BaseReplyHandler
+
+        ALPHA_ORDINALS = ('A'..'Z').to_a.freeze
 
         attr_reader :recipient_id, :reply
 
@@ -17,7 +18,35 @@ module Stealth
         def text
           check_text_length
 
-          format_response({ body: reply['text'] })
+          translated_reply = reply['text']
+
+          suggestions = generate_suggestions(suggestions: reply['suggestions'])
+          buttons = generate_buttons(buttons: reply['buttons'])
+
+          if suggestions.present?
+            translated_reply = [
+              translated_reply,
+              'Reply with one of the following:'
+            ].join("\n\n")
+
+            suggestions.each_with_index do |suggestion, i|
+              translated_reply = [
+                translated_reply,
+                "\"#{ALPHA_ORDINALS[i]}\" for #{suggestion}"
+              ].join("\n")
+            end
+          end
+
+          if buttons.present?
+            buttons.each do |button|
+              translated_reply = [
+                translated_reply,
+                button
+              ].join("\n\n")
+            end
+          end
+
+          format_response({ body: translated_reply })
         end
 
         def image
@@ -52,17 +81,46 @@ module Stealth
 
           def check_text_length
             if reply['text'].present? && reply['text'].size > 1600
-              raise(ArgumentError, "Text messages must be 1600 characters or less.")
+              raise(ArgumentError, 'Text messages must be 1600 characters or less.')
             end
           end
 
           def format_response(response)
-            sender_info = { from: Stealth.config.twilio.from_phone, to: recipient_id }
+            sender_info = {
+              from: Stealth.config.twilio.from_phone,
+              to: recipient_id
+            }
             response.merge(sender_info)
           end
 
-      end
+          def generate_suggestions(suggestions:)
+            return if suggestions.blank?
 
+            mf = suggestions.collect do |suggestion|
+              suggestion['text']
+            end.compact
+          end
+
+          def generate_buttons(buttons:)
+            return if buttons.blank?
+
+            sms_buttons = buttons.map do |button|
+              case button['type']
+              when 'url'
+                "#{button['text']}: #{button['url']}"
+              when 'payload'
+                "To #{button['text']}: Text #{button['payload'].upcase}"
+              when 'call'
+                "#{button['text']}: #{button['phone_number']}"
+              else # Don't raise for unsupported buttons
+                next
+              end
+            end.compact
+
+            sms_buttons
+          end
+
+      end
     end
   end
 end
