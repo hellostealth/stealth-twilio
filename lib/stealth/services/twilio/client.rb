2744d0b7679cca4c8fb4f5eb5e7d5cdac90eae9f
Mauricio Gomes <mauricio@edge14.com>
2022-03-02 17:38:04 -0500

    Handle additional Twilio errors (#85)
    * Handle additional Twilio errors
    
    * Invalid phone numbers or landlines return InvalidSessionId

--- lib/stealth/services/twilio/client.rb
+++ lib/stealth/services/twilio/client.rb
@@ -39,18 +39,24 @@ module Stealth
             case e.message
             when /21610/ # Attempt to send to unsubscribed recipient
               raise Stealth::Errors::UserOptOut
-            when /21612/ # 'To' phone number is not currently reachable via SMS
-              raise Stealth::Errors::UserOptOut
-            when /21614/ # 'To' number is not a valid mobile number
-              raise Stealth::Errors::UserOptOut
-            when /30004/ # Message blocked
-              raise Stealth::Errors::UserOptOut
             when /21211/ # Invalid 'To' Phone Number
+              raise Stealth::Errors::InvalidSessionID
+            when /21612/ # 'To' phone number is not currently reachable via SMS
+              raise Stealth::Errors::InvalidSessionID
+            when /21614/ # 'To' number is not a valid mobile number
               raise Stealth::Errors::InvalidSessionID
             when /30003/ # Unreachable destination handset
               raise Stealth::Errors::InvalidSessionID
+            when /30004/ # Message Blocked
+              raise Stealth::Errors::MessageFiltered
             when /30005/ # Unknown destination handset
               raise Stealth::Errors::InvalidSessionID
+            when /30006/ # Landline or unreachable carrier
+              raise Stealth::Errors::InvalidSessionID
+            when /30007/ # Message filtered
+              raise Stealth::Errors::MessageFiltered
+            when /30008/ # Unknown error 🤷‍♂️
+              raise Stealth::Errors::UnknownServiceError
             else
               raise
             end
