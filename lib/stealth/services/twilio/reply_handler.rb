508e91311d5c1eedb02a37812160897a7275fff0
Mauricio Gomes <mauricio@edge14.com>
2022-11-05 20:04:27 -0400

    Expose translated replies in the public API (#87)
    * Expose the translated reply in the public API
    
    * Bump to 1.5.0

--- lib/stealth/services/twilio/reply_handler.rb
+++ lib/stealth/services/twilio/reply_handler.rb
@@ -8,7 +8,7 @@ module Stealth
 
         ALPHA_ORDINALS = ('A'..'Z').to_a.freeze
 
-        attr_reader :recipient_id, :reply
+        attr_reader :recipient_id, :reply, :translated_reply
 
         def initialize(recipient_id: nil, reply: nil)
           @recipient_id = recipient_id
@@ -18,20 +18,20 @@ module Stealth
         def text
           check_text_length
 
-          translated_reply = reply['text']
+          @translated_reply = reply['text']
 
           suggestions = generate_suggestions(suggestions: reply['suggestions'])
           buttons = generate_buttons(buttons: reply['buttons'])
 
           if suggestions.present?
-            translated_reply = [
-              translated_reply,
+            @translated_reply = [
+              @translated_reply,
               'Reply with:'
             ].join("\n\n")
 
             suggestions.each_with_index do |suggestion, i|
-              translated_reply = [
-                translated_reply,
+              @translated_reply = [
+                @translated_reply,
                 "\"#{ALPHA_ORDINALS[i]}\" for #{suggestion}"
               ].join("\n")
             end
@@ -39,14 +39,14 @@ module Stealth
 
           if buttons.present?
             buttons.each do |button|
-              translated_reply = [
-                translated_reply,
+              @translated_reply = [
+                @translated_reply,
                 button
               ].join("\n\n")
             end
           end
 
-          format_response({ body: translated_reply })
+          format_response({ body: @translated_reply })
         end
 
         def image
